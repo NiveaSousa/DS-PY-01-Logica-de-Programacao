124edc11595cc146681574adedcc41655872cd1c
Nivea Ester <88627342+NiveaSousa@users.noreply.github.com>
2022-09-29 15:48:14 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,7 +1,61 @@
 import random
-# Declare suas funções abaixo
+
+moveis = ['Marta','Gustavo', 'Antônio', 'Joana', 'Rodrigo', 'Teodoro', 'Cristina',
+'Benício', 'Artur', 'Conrado', 'Betânia', 'Estevan', 'Gabriele', 'Vicente', 'Vitório']
+
+palavra = random.choice(moveis)
+palavra = palavra.upper()
+
+linha = len(palavra)*'_'
+print('Adivinhe a Palavra:\nCategoria: Nome de pessoa\nSe a palavra tem até 5 letras, você tem 4 palpites, caso tenha mais, você tem 6. \nBoa sorte')
+print(linha)
+
+if len(palavra)<=5:
+    nova_linha = linha
+    for i in range (3):
+        guess = input('Digite uma letra\n').upper()
+        if guess in palavra:
+            canto = palavra.find(guess)
+            resto = palavra[canto+1:]
+            if guess in resto:
+                onde = resto.find(guess)
+                dif = len(palavra)-len(resto)
+                nova_linha = nova_linha[:canto] + guess + nova_linha[(canto+1):(onde+dif)]+guess+ nova_linha[(onde+dif+1):]
+            else:
+                nova_linha = nova_linha[:canto] + guess + nova_linha[(canto+1):]
+            print(nova_linha)
+        else:
+            print('Não faz parte da palavra')
+            
+    else:
+        guess = input('Ultima tentativa. Descubra a palavra\n').upper()
+        if guess == palavra:
+            print(f'Parabéns! Você acertou o nome {palavra}')
+        else:
+            print('Uma pena, você não acertou')
 
 
-if __name__ == "__main__":
-    # Adicione seu código abaixo e declare suas funções a serem usadas acima
-    print("Código a ser executado!")
+else:
+    nova_linha = linha
+    for i in range(5):
+        guess = input('Digite uma letra\n').upper()
+        if guess in palavra:
+            canto = palavra.find(guess)
+            resto = palavra[canto+1:]
+            if guess in resto:
+                onde = resto.find(guess)
+                dif = len(palavra)-len(resto)
+                nova_linha = nova_linha[:canto] + guess + nova_linha[(canto+1):(onde+dif)]+guess+ nova_linha[(onde+dif+1):]
+            else:
+                nova_linha = nova_linha[:canto] + guess + nova_linha[(canto+1):]
+            print(nova_linha)
+            
+        else:
+            print('Não faz parte da palavra\n')
+    else:
+        guess = input('Ultima tentativa. Descubra a palavra\n').upper()
+        if guess == palavra:
+            print(f'Parabéns! Você acertou o nome {palavra}')
+        else:
+            print('Uma pena, você não acertou')
+
